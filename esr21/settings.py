356fd213dcb90df557077f29c6726b4b1b6f793b
ame diphoko <diphokoame@gmail.com>
2021-06-09 08:26:25 +0200

    update date and time input format

--- esr21/settings.py
+++ esr21/settings.py
@@ -174,9 +174,15 @@ TIME_ZONE = 'Africa/Gaborone'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
+
+DATETIME_INPUT_FORMATS = ['%d/%m/%Y %H:%M']
+DATE_INPUT_FORMATS = ['%d/%m/%Y']
+TIME_INPUT_FORMATS = ['%H:%M']
+DATETIME_FORMAT = 'd/M/Y H:i'
+DATE_FORMAT = 'd/M/Y'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
