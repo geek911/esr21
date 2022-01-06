24a3a9d2c1f0539f5e0c85170f7140c32238fb55
ame diphoko <diphokoame@gmail.com>
2022-01-06 08:47:28 +0200

    revert home view changes, add filter by site id

--- esr21/views/home_view.py
+++ esr21/views/home_view.py
@@ -27,16 +27,19 @@ class HomeView(EdcBaseViewMixin, NavbarViewMixin, TemplateView):
     def vaccine_model_cls(self):
         return django_apps.get_model(self.vaccine_model)
 
-    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        subject_screening = self.subject_screening_cls.objects.all()
-        subject_consent = self.subject_consent_cls.objects.all()
+        subject_screening = self.subject_screening_cls.objects.filter(
+            site__id=settings.SITE_ID)
+        subject_consent = self.subject_consent_cls.objects.filter(
+            site__id=settings.SITE_ID)
         vaccinated_first_dose = self.vaccine_model_cls.objects.filter(
-            received_dose='Yes', received_dose_before='first_dose')
-    
+            received_dose='Yes', received_dose_before='first_dose',
+            site__id=settings.SITE_ID)
+
         vaccinated_second_dose = self.vaccine_model_cls.objects.filter(
-            received_dose='Yes', received_dose_before='second_dose')
+            received_dose='Yes', received_dose_before='second_dose',
+            site__id=settings.SITE_ID)
 
         screened_subjects = subject_screening.count()
         consented_subjects = subject_consent.count()
