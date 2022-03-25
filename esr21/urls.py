cd8563becddb92af3d063c77d1634d2c8f60ce02
McTurner45 <58169969+nmunatsi@users.noreply.github.com>
2022-03-25 09:58:46 +0200

    Update urls.py

--- esr21/urls.py
+++ esr21/urls.py
@@ -36,6 +36,8 @@ from edc_sync.admin_site import edc_sync_admin
 from edc_sync_files.admin_site import edc_sync_files_admin
 from edc_visit_schedule.admin_site import edc_visit_schedule_admin
 from esr21_export.admin_site import esr21_export_admin
+
+from esr21_pharmacy.admin_site import esr21_pharmacy_admin
 from esr21_prn.admin_site import esr21_prn_admin
 from esr21_follow.admin_site import esr21_follow_admin
 from esr21_subject.admin_site import esr21_subject_admin
@@ -63,6 +65,7 @@ urlpatterns = [
     path('admin/', edc_sync_files_admin.urls),
 
     path('admin/', esr21_follow_admin.urls),
+    path('admin/', esr21_pharmacy_admin.urls),
     path('admin/', esr21_subject_admin.urls),
     path('admin/', esr21_prn_admin.urls),
     path('admin/', esr21_export_admin.urls),
@@ -108,7 +111,7 @@ urlpatterns = [
     path('edc_sync_files/', include('edc_sync_files.urls')),
 
     path('subject/', include('esr21_dashboard.urls')),
-    path('edc_meddra/', include('edc_meddra.urls')),
+    path('pharma/', include('esr21_pharmacy_dashboard.urls')),
 
     path('switch_sites/', LogoutView.as_view(next_page=settings.INDEX_PAGE),
          name='switch_sites_url'),
