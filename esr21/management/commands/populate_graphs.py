5696a809d312c09fcbceea5c150f3c634583befe
MoffatMore <mofenyimoffat@gmail.com>
2022-06-13 21:31:12 +0200

    update populate management command

--- esr21/management/commands/populate_graphs.py
+++ esr21/management/commands/populate_graphs.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand
 from esr21_reports.views.graphs_mixins import (AgeDistributionGraphMixin,
                                                ScreeningGraphMixin,
-                                               EnrollmentGraphMixin)
+                                               EnrollmentGraphMixin,
+                                               VaccinationGraphMixin)
 from esr21_reports.models import (
     AgeStatistics, ScreeningStatistics, EnrollmentStatistics,
     VaccinationStatistics)
@@ -17,18 +18,19 @@ class Command(BaseCommand):
         self.populate_age_graph()
         self.populate_screening_data()
         self.populate_enrollement_data()
+        self.populate_vaccination_data()
 
     def populate_age_graph(self):
         age_distribution = AgeDistributionGraphMixin()
         for site in self.siteHelper.sites_names:
-            age_dist = age_distribution.get_distribution_site(site)
+            min, lowerquartile, median, upperquartile, max = age_distribution.get_distribution_site(site)
             AgeStatistics.objects.update_or_create(
                 site=site,
-                min=age_dist[0],
-                lowerquartile=age_dist[1],
-                median=age_dist[2],
-                upperquartile=age_dist[3],
-                max=age_dist[4],
+                min=min,
+                lowerquartile=lowerquartile,
+                median=median,
+                upperquartile=upperquartile,
+                max=max,
             )
 
     def populate_other_graphs(self):
@@ -37,17 +39,19 @@ class Command(BaseCommand):
     def populate_screening_data(self):
         screening = ScreeningGraphMixin()
         for site in self.siteHelper.sites_names:
-            results = screening.get_screened_by_site(site_name_postfix=site)
+            site_id = self.siteHelper.get_site_id(site)
+            passed, failed = screening.get_screened_by_site(site_id=site_id)
             ScreeningStatistics.objects.update_or_create(
                 site=site,
-                passed=results[0],
-                failed=results[1]
+                passed=passed,
+                failed=failed
             )
 
     def populate_enrollement_data(self):
         enrollment = EnrollmentGraphMixin()
         for site in self.siteHelper.sites_names:
-            male, female = enrollment.get_vaccinated_by_site(site)
+            site_id = self.siteHelper.get_site_id(site)
+            male, female = enrollment.get_vaccinated_by_site(site_id)
             total = male + female
             EnrollmentStatistics.objects.update_or_create(
                 site=site,
@@ -55,3 +59,21 @@ class Command(BaseCommand):
                 male=male,
                 female=female
             )
+
+    def populate_vaccination_data(self):
+        vaccine = VaccinationGraphMixin()
+        for site in self.siteHelper.sites_names:
+            site_id = self.siteHelper.get_site_id(site)
+            first_dose = vaccine.site_dose_vaccination(
+                site_id=site_id, dose='first_dose')
+            second_dose = vaccine.site_dose_vaccination(
+                site_id=site_id, dose='first_dose')
+            booster_dose = vaccine.site_dose_vaccination(
+                site_id=site_id, dose='first_dose')
+            VaccinationStatistics.objects.update_or_create(
+                site=site,
+                dose_1=first_dose,
+                dose_2=second_dose,
+                dose_3=booster_dose
+            )
+        pass
